9c94b9e6678d3336322a2ec9e34be0ddd0ccb561
ray <mathew177@gmail.com>
2018-10-23 16:35:13 +0800

    remove contain script

--- remove_tspv2.sh
+++
@@ -1,9 +0,0 @@
-docker rm -f $(docker ps -a | grep tspv2_api_ad | awk '{print $1 }')
-docker rmi -f $(docker images -q tspv2_api_ad)
-docker rmi -f $(docker images -q asia.gcr.io/tspv1-188510/tspv2_api_ad)
-
-docker rm -f $(docker ps -a | grep tspv2_api_proxy | awk '{print $1 }')
-docker rmi -f $(docker images -q tspv2_api_proxy)
-docker rmi -f $(docker images -q asia.gcr.io/tspv1-188510/tspv2_api_proxy)
-
-
